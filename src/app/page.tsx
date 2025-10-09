70be73d1c11cf856e0d479d6af5024975a725e7c
Charly Martin <charly@hey.com>
2025-10-09 09:39:54 +0200

    fixup! style(Home): update introductory text for improved clarity and engagement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,11 @@ export default function Home() {
       <div className="mx-auto flex flex-col items-center gap-15 text-center">
         <Announcement>Coming soon</Announcement>
 
-        <div className="max-w-2xl space-y-6">
+        <div className="space-y-6">
           <h1 className="font-heading text-4xl font-medium text-pretty md:text-6xl">
             Filecoin Onchain Cloud
           </h1>
-          <p className="text-xl text-pretty text-zinc-300">
+          <p className="mx-auto max-w-md text-center text-xl text-pretty text-zinc-300">
             The future of the cloud starts with all of us. Are you ready to take
             back the cloud?
           </p>
