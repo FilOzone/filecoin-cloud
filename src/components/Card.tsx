7cd158a3ceeda248f0a72bdf3482cab1a22cc263
Charly Martin <charly@hey.com>
2025-10-14 11:53:53 +0100

    style: adjust card heading vertical position from top-9 to top-10
    Minor styling tweak to improve the vertical alignment of the card's heading element for better visual spacing.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -26,7 +26,7 @@ export function Card({ text, icon: IconComponent, link }: CardProps) {
           href={link}
           className="absolute inset-0 text-zinc-50 focus:outline-none"
         >
-          <h3 className="absolute top-9 left-14 font-sans text-lg font-medium">
+          <h3 className="absolute top-10 left-14 font-sans text-lg font-medium">
             {text}
           </h3>
         </ExternalLink>
