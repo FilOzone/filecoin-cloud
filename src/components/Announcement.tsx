f53920d696edbea6ad8ef33d63c3002307523e23
Charly Martin <charly@hey.com>
2025-10-21 11:55:22 +0100

    refactor(Announcement): replace <output> with <div> and remove aria-live for semantic accuracy

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -4,11 +4,8 @@ export type AnnouncementProps = {
 
 export function Announcement({ children }: AnnouncementProps) {
   return (
-    <output
-      className="rounded-full border border-zinc-700/80 bg-zinc-800/80 px-5 py-2.5 font-sans"
-      aria-live="polite"
-    >
+    <div className="rounded-full border border-zinc-700/80 bg-zinc-800/80 px-5 py-2.5 font-sans">
       <p className="font-medium text-zinc-50">{children}</p>
-    </output>
+    </div>
   )
 }
