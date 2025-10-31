8d21230d50332c6a14aa25a85e2657b422ce17bc
Barbara Peric <b.peric10@gmail.com>
2025-10-31 13:46:29 +0000

    Enhance ContractCard with external link and update ContractCardGrid layout
    - Added an external link to the ContractCard component for viewing contract addresses on explorers.
    - Updated the layout of ContractCardGrid to use auto column sizing for better responsiveness.

--- src/app/hidden/warm-storage-service/components/ContractCard.tsx
+++ src/app/hidden/warm-storage-service/components/ContractCard.tsx
@@ -1,17 +1,26 @@
+import { ExternalLink } from '@/components/ExternalLink'
+
 import { shortenAddress } from '../utils/shortenAddress'
 
 type ContractCardProps = {
   label: string
   address: string
+  href: string
 }
 
-export function ContractCard({ label, address }: ContractCardProps) {
+export function ContractCard({ label, address, href }: ContractCardProps) {
   return (
-    <div className="border border-(--color-border-base) rounded-2xl p-6 flex flex-col gap-1.5">
+    <div className="border relative border-(--color-border-base) p-6 flex flex-col gap-1.5 hover:bg-zinc-50 rounded-2xl focus-within:bg-zinc-50 focus-within:border-brand-600 cursor-pointer">
       <p className="text-(--color-paragraph-text)">{label}</p>
       <span className="text-brand-800 font-medium">
         {shortenAddress({ address })}
       </span>
+      <ExternalLink href={href}>
+        <span className="sr-only">
+          View {label} contract address {shortenAddress({ address })} on
+          explorer
+        </span>
+      </ExternalLink>
     </div>
   )
 }
--- src/app/hidden/warm-storage-service/components/ContractCardGrid.tsx
+++ src/app/hidden/warm-storage-service/components/ContractCardGrid.tsx
@@ -4,7 +4,7 @@ type ContractCardGridProps = {
 
 export function ContractCardGrid({ children }: ContractCardGridProps) {
   return (
-    <div className="grid lg:grid-rows-1 lg:grid-cols-4 gap-4 grid-cols-1 sm:grid-cols-2">
+    <div className="grid lg:grid-rows-1 lg:grid-flow-col lg:auto-cols-[1fr] gap-4 grid-cols-1 sm:grid-cols-2">
       {children}
     </div>
   )
