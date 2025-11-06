4481a6dbfa92c2e673e335d5ee7b44afc22355cb
Charly Martin <charly@hey.com>
2025-11-06 12:02:11 +0000

    Fix arrow icon positioning

--- src/components/CompactAddress.tsx
+++ src/components/CompactAddress.tsx
@@ -14,6 +14,7 @@ export function CompactAddress({ address }: CompactAddressProps) {
       href={`${EXPLORERS.BLOCKSCOUT.calibration}${address}`}
       aria-label={`View address ${address} on Block Explorer`}
       title={address}
+      className="whitespace-nowrap"
     >
       <span className="font-medium">{truncateAddress(address)}</span>
     </ExternalTextLink>
