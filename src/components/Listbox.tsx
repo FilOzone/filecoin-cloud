bc31fc2558dbe8b55e5906ec88be4f84a0c44471
Charly Martin <charly@hey.com>
2025-11-07 10:02:47 +0000

    Add dynamic background colors

--- src/components/Listbox.tsx
+++ src/components/Listbox.tsx
@@ -1,4 +1,11 @@
+'use client'
+
 import { Icon } from '@filecoin-foundation/ui-filecoin/Icon'
+import { variantMapping } from '@filecoin-foundation/ui-filecoin/Navigation/constants'
+import {
+  backgroundVariants,
+  useBackgroundVariant,
+} from '@filecoin-foundation/ui-filecoin/Section'
 import {
   Listbox as HeadlessListbox,
   ListboxButton,
@@ -7,6 +14,7 @@ import {
 } from '@headlessui/react'
 import type { Icon as IconType } from '@phosphor-icons/react'
 import { CaretDownIcon } from '@phosphor-icons/react/dist/ssr'
+import { clsx } from 'clsx'
 
 type Option = {
   id: string
@@ -26,9 +34,12 @@ export function Listbox({
   setSelected,
   Icon: IconComponent,
 }: LisboxProps) {
+  const backgroundVariant = useBackgroundVariant()
+  const desktopBackgroundVariant = variantMapping[backgroundVariant]
+
   return (
     <HeadlessListbox value={selected} onChange={setSelected}>
-      <ListboxButton className="relative block min-w-56 rounded-lg bg-transparent p-3 text-left font-semibold text-(--color-navigation-link-text) data-focus:brand-outline border border-(--color-listbox-border)">
+      <ListboxButton className="relative block min-w-56 rounded-lg bg-transparent p-3 text-left font-semibold text-(--color-lisbox-button-text) data-focus:brand-outline focus:brand-outline focus-visible:brand-outline border border-(--color-listbox-border) data-focus:bg-(--color-listbox-button-background)">
         <span className="flex gap-2 items-center">
           <Icon component={IconComponent} size={20} />
           {selected.label}
@@ -41,15 +52,18 @@ export function Listbox({
       <ListboxOptions
         transition
         anchor="bottom start"
-        className="w-(--button-width) rounded-lg border border-(--color-listbox-border) p-2 [--anchor-gap:--spacing(1)] focus:outline-none transition duration-100 ease-in data-leave:data-closed:opacity-0 space-y-2 shadow-xs"
+        className={clsx(
+          backgroundVariants[desktopBackgroundVariant],
+          'w-(--button-width) rounded-lg border border-(--color-listbox-border) p-2 [--anchor-gap:--spacing(2)] focus:outline-none transition duration-100 ease-in data-leave:data-closed:opacity-0 space-y-2 shadow-xs',
+        )}
       >
         {options.map((option) => (
           <ListboxOption
             key={option.label}
             value={option}
-            className="group flex cursor-default items-center gap-2 rounded-md px-4 py-3 select-none data-selected:bg-(--color-listbox-option-background) data-focus:bg-(--color-listbox-option-background)"
+            className="group flex cursor-default items-center gap-2 rounded-md px-4 py-3 select-none data-focus:bg-(--color-listbox-option-background-hover)! data-selected:bg-(--color-listbox-option-background)"
           >
-            <div className="font-semibold text-(--color-navigation-link-text)">
+            <div className="font-semibold text-(--color-lisbox-button-text)">
               {option.label}
             </div>
           </ListboxOption>
