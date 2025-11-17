c484d2803e805393b5cadb63e4d57e1486c0a62f
Charly Martin <charly@hey.com>
2025-11-17 16:05:19 +0000

    Relocate desktopStyle import to local NavigationMainLink

--- src/components/Navigation/NavigationMenu/NavigationMenu.tsx
+++ src/components/Navigation/NavigationMenu/NavigationMenu.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { desktopStyle } from '@filecoin-foundation/ui-filecoin/Navigation/NavigationMainLink'
 import { NavigationMenu as SharedNavigationMenu } from '@filecoin-foundation/ui-filecoin/Navigation/NavigationMenu'
 import { NavigationMenuPanel } from '@filecoin-foundation/ui-filecoin/Navigation/NavigationMenuPanel'
 import clsx from 'clsx'
@@ -8,6 +7,7 @@ import clsx from 'clsx'
 import { NavigationMenuLink } from './NavigationMenuLink'
 import type { NavigationMenuItem } from '../constants/navigation'
 import { useIsNavigationMenuActive } from '../hooks/use-navigation-menu-active'
+import { desktopStyle } from '../NavigationMainLink'
 
 export function NavigationMenu({ label, items }: NavigationMenuItem) {
   const isActive = useIsNavigationMenuActive(items)
