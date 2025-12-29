1bde63bc72cc171e10f25a1f3f49272acacc1d10
Mirha Masala <26620750+mirhamasala@users.noreply.github.com>
2025-12-29 06:23:49 +0000

    Revert "Konami Santa"

--- src/components/Navigation/HomeLogoIconLink.tsx
+++ src/components/Navigation/HomeLogoIconLink.tsx
@@ -3,20 +3,11 @@
 import { HomeLogoLink } from '@filecoin-foundation/ui-filecoin/HomeLogoLink'
 import { useBackground } from '@filecoin-foundation/ui-filecoin/Section/Section'
 
-import { SantaHat } from '@/components/SantaHat'
-
-import { useKonamiCode } from '@/hooks/use-konami-code'
 import LogoDark from '@/public/assets/logos/foc-logo-dark.svg'
 import LogoLight from '@/public/assets/logos/foc-logo-light.svg'
 
 export function HomeLogoIconLink() {
   const { isLight } = useBackground()
-  const { showHat } = useKonamiCode()
 
-  return (
-    <div className="relative">
-      <HomeLogoLink logo={isLight ? LogoDark : LogoLight} height={40} />
-      <SantaHat show={showHat} />
-    </div>
-  )
+  return <HomeLogoLink logo={isLight ? LogoDark : LogoLight} height={40} />
 }
