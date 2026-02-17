112ec5b02ae38e43e1098c0de30a35db7cd04a1f
Mirha Masala <mirha@hey.com>
2026-02-17 17:12:09 +0000

    Create sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,14 +1,34 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next";
 
-import { BASE_URL } from '@/constants/site-metadata'
+import { getAllRFSData } from "@/app/agents/utils/get-rfs-data";
+import { PATHS } from "@/constants/paths";
+import { BASE_URL } from "@/constants/site-metadata";
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: BASE_URL,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 1,
-    },
-  ]
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const staticRoutes = getStaticRoutes();
+  const agentRoutes = await getAgentRoutes();
+
+  return [...staticRoutes, ...agentRoutes];
+}
+
+const lastModified = new Date();
+
+function getStaticRoutes() {
+  return Object.values(PATHS).map(({ path }) => ({
+    url: `${BASE_URL}${path}`,
+    lastModified,
+    changeFrequency: "weekly" as const,
+    priority: path === "/" ? 1 : 0.8,
+  }));
+}
+
+async function getAgentRoutes() {
+  const allRFSData = await getAllRFSData();
+
+  return allRFSData.map(({ slug }) => ({
+    url: `${BASE_URL}${PATHS.AGENTS.path}/${slug}`,
+    lastModified,
+    changeFrequency: "weekly" as const,
+    priority: 0.7,
+  }));
 }
