1807b93584a4bdce0a334f73e42d4bdb7d2cb2d5
Mirha Masala <mirha@hey.com>
2026-02-18 08:58:54 +0000

    CR

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,34 +1,25 @@
 import type { MetadataRoute } from "next";
-
 import { getAllRFSData } from "@/app/agents/utils/get-rfs-data";
 import { PATHS } from "@/constants/paths";
 import { BASE_URL } from "@/constants/site-metadata";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const staticRoutes = getStaticRoutes();
-  const agentRoutes = await getAgentRoutes();
+  const lastModified = new Date();
 
-  return [...staticRoutes, ...agentRoutes];
-}
-
-const lastModified = new Date();
-
-function getStaticRoutes() {
-  return Object.values(PATHS).map(({ path }) => ({
+  const staticRoutes = Object.values(PATHS).map(({ path }) => ({
     url: `${BASE_URL}${path}`,
     lastModified,
     changeFrequency: "weekly" as const,
     priority: path === "/" ? 1 : 0.8,
   }));
-}
 
-async function getAgentRoutes() {
   const allRFSData = await getAllRFSData();
-
-  return allRFSData.map(({ slug }) => ({
+  const agentRoutes = allRFSData.map(({ slug }) => ({
     url: `${BASE_URL}${PATHS.AGENTS.path}/${slug}`,
     lastModified,
     changeFrequency: "weekly" as const,
     priority: 0.7,
   }));
+
+  return [...staticRoutes, ...agentRoutes];
 }
