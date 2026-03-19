23e35a1e2509e872fe9787f91a5e5912d2f4a741
Barbara Peric <b.peric10@gmail.com>
2026-03-19 14:44:46 +0000

    Add GitHub link to service providers and footer legal links

--- src/components/Footer/data/legal-links.ts
+++ src/components/Footer/data/legal-links.ts
@@ -1,4 +1,5 @@
 import { PATHS } from '@/constants/paths'
+import { FOC_URLS } from '@/constants/site-metadata'
 
 type LegalLink = {
   label: string
@@ -11,4 +12,5 @@ export const legalLinks: Array<LegalLink> = [
     href: PATHS.PRIVACY_POLICY.path,
   },
   { label: PATHS.TERMS_OF_USE.label, href: PATHS.TERMS_OF_USE.path },
+  { label: 'GitHub', href: FOC_URLS.social.github },
 ]
